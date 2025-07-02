cb3b78a63601079c20cbf0f5069ff883bf3f81f1
Ravish052 <bhatnagar463@gmail.com>
2025-07-02 21:30:31 +0530

    passport auth implemented

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ import cors from "cors";
 import dotenv from "dotenv";;
 import { ApolloServer } from "@apollo/server";
 
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import {connectDB} from "./db/connectDB.js";
@@ -12,9 +16,37 @@ import {connectDB} from "./db/connectDB.js";
 import mergedResolver from "./resolvers/index.js";
 import mergedTypeDef from "./typeDefs/index.js";
 
+import { buildContext } from "graphql-passport";
+
 dotenv.config();
 const app = express();
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: "sessions",
+});
+
+store.on("error", (error) => {
+    console.error("Session store error:", error);
+});
+
+app.use(session(
+    {
+        secret : process.env.SESSION_SECRET,
+        resave : false, // Do not save session if unmodified
+        saveUninitialized : false,
+        cookie : {
+            maxAge : 7* 24 * 60 * 60 * 1000, // 7 days
+            httpOnly : true, // Prevent client-side JavaScript from accessing the cookie
+        },
+        store : store
+    }
+))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
     typeDefs: mergedTypeDef,
@@ -31,7 +63,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/", expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req , res}) => buildContext({ req , res}),
 }));
 
 
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -4,7 +4,7 @@ const userResolver = {
         users : ()=>{
             return users;
         },
-        user : (_,{userId}) =>{
+        user : (_,{userId},) =>{
             return users.find(user => user._id === userId);
         }
     },
